cd7d370cc8b65cbbb97f99af252d0b53bc7228df
Sam Wilson <iduckyfuzz@gmail.com>
2021-01-04 16:56:49 +0000

    end of the day

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = 5000;
 const path = require('path');
 const hbs = require('hbs');
 require('dotenv').config();
+const User = require('./models/user');
+const bcrypt = require('bcryptjs');
 
 //connect to mongodb database
 mongoose.connect(process.env.DB_URL, {
@@ -41,17 +43,75 @@ app.set('view engine', 'hbs');
 //set express to use the static files
 app.use(express.static(publicDirectory));
 
-app.get('/', (req, res) => {
-    res.render("index");
+app.get('/', async (req, res) => {
+
+    const users = await User.find();
+    res.render("index", {
+        users: users
+    });
 });
 
 app.get('/register', (req, res) => {
     res.render("register");
 });
 
-app.post('/register', (req, res)=>  {
-    console.log(req.body)
-    res.render("register");
+app.get('/profile/:id', async (req, res) => {
+    //could use a try catch here 
+    const user = await User.findById(req.params.id);
+    res.render("profile", {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+    });
+});
+
+app.get('/profile/update/:id', async (req, res) => {
+    //could use a try catch if the user doesn't exist
+    const userName = "Jill";
+    const userEmail = "Jill@gmail.com";
+
+    await User.findByIdAndUpdate(req.params.id, {
+        name: userName,
+        email: userEmail
+    })
+
+    res.send("Update Succesful!");
+});
+
+
+app.post('/delete/:id', async (req, res) => {
+    //could use a try catch if the user doesn't exist
+    await User.findByIdAndDelete(req.params.id)
+
+    res.send("Delete Succesful!");
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.post('/login', async (req, res) => {
+    //add bycrypt compare of provided user & password
+    const error = "login failed";
+    res.render("login", {
+        error: error
+    });
+});
+
+app.post('/register', async (req, res) => {
+
+    //hash the password
+    let hashedPassword = await bcrypt.hash(req.body.pword, 8);
+    await User.create({
+        name: req.body.userName,
+        email: req.body.userEmail,
+        password: hashedPassword
+    })
+
+
+
+    res.send("User registered");
 });
 
 app.get('*', (req, res) => {
